6329f53a1a80b346f0c1676ef9511175b97f42ac
barmalry84 <oleksandr.bukhanko@gmail.com>
2024-01-24 11:29:41 +0100

    Some

--- iac/app_iac/redis.tf
+++ iac/app_iac/redis.tf
@@ -9,7 +9,7 @@ resource "aws_security_group" "people_info_api_sg" {
   name        = "people-info-api-redis-sg"
   description = "Security group for Redis cluster"
 
-  vpc_id      = data.aws_vpc.precreated_vpc.id
+  vpc_id = data.aws_vpc.precreated_vpc.id
 
   ingress {
     from_port   = 6379
@@ -31,6 +31,6 @@ resource "aws_elasticache_cluster" "people_info_api_cluster" {
   num_cache_nodes      = 1
   parameter_group_name = "default.redis7"
 
-  security_group_ids   = [aws_security_group.people_info_api_sg.id]
-  subnet_group_name    = aws_elasticache_subnet_group.redis_subnet_group.name
+  security_group_ids = [aws_security_group.people_info_api_sg.id]
+  subnet_group_name  = aws_elasticache_subnet_group.redis_subnet_group.name
 }
